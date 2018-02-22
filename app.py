9f354fdb21b867b3d2ee74afaca16452fcbcd7c4
yogesh <dharneyogesh@gmail.com>
2018-02-22 17:22:08 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import paramiko
+import getpass
+
+hosts = input('Enter Your Hostname or Host IP: ')
+user = input('Enter Your Login Username: ')
+password = input('Enter Your Login Password: ')
+command = input("Enter target command:")
+
+ssh = paramiko.SSHClient() #Define value 'ssh' as calling the paramiko.sshclient
+#print('calling paramiko')
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Must come before connect line to add hosts to known_hosts
+#ssh.connect("name", username="user", password="passwd")
+ssh.connect(hostname = hosts, username = user, password = password) # key_filename means to specify the actual file, pkey on the
+#other hand means to actually paste in the key text itself
+print('trying to connect')
+ssh.invoke_shell()
+stdin, stdout, stderr = ssh.exec_command (command)
+print(stdout.read())
+#ssh.close()
